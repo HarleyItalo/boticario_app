c59fbc2631ce9b06dd5d24a769cc1c92d5293215
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2022-03-09 12:25:30 -0300

    adicionando o servico de navegacao

---
+++ lib/common/services/navigation_service.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class NavigationService {
+  static final GlobalKey<NavigatorState> navigatorKey =
+      GlobalKey<NavigatorState>();
+
+  static void pushNamed(
+    String name, {
+    Object? args,
+  }) async {
+    await navigatorKey.currentState?.pushNamed(
+      name,
+      arguments: args,
+    );
+  }
+
+  static void push(
+    page,
+  ) async {
+    await navigatorKey.currentState?.push(
+      MaterialPageRoute(
+        builder: (context) => page,
+      ),
+    );
+  }
+
+  static void pushNamedAndRemoveUntil(
+    String name, {
+    Object? args,
+  }) async {
+    navigatorKey.currentState
+        ?.pushNamedAndRemoveUntil(name, (route) => false, arguments: args);
+  }
+
+  static void pushReplacementNamed(
+    String name, {
+    Object? args,
+  }) async {
+    navigatorKey.currentState?.pushReplacementNamed(name, arguments: args);
+  }
+
+  static void pop() {
+    if (navigatorKey.currentState?.canPop() ?? false) {
+      navigatorKey.currentState?.pop();
+    }
+  }
+
+  static bool canPop() {
+    return navigatorKey.currentState?.canPop() ?? false;
+  }
+
+  static getArgs(context) {
+    var args = ModalRoute.of(context)!.settings.arguments;
+    return args;
+  }
+}
