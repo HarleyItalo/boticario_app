ebda3bfc23b87f6c6549f83567d002b3f312358d
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2022-03-10 03:25:36 -0300

    fazendo os testes de falha de login

---
+++ test/mocks/make_login_mock.dart
@@ -0,0 +1,4 @@
+import 'package:boticario_app/modules/login/domain/user_cases/make_login_user_case.dart';
+import 'package:mockito/mockito.dart';
+
+class LoginRepositoryMock extends Mock implements MakeLoginImpl {}
--- test/modules/login/domain/user_cases/make_login_test.dart
+++ test/modules/login/domain/user_cases/make_login_test.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'package:boticario_app/common/services/http_service.dart';
+import 'package:boticario_app/modules/login/domain/errors/not_logged_exception.dart';
+import 'package:boticario_app/modules/login/domain/user_cases/make_login_user_case.dart';
+import 'package:boticario_app/modules/login/infra/repositories/login_repository_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final dioClient = Dio();
+  final dioMock =
+      DioHttpService(dioClient, "https://622925e0be12fc4538979da3.mockapi.io/");
+  final _repository = LoginRepositoryImpl(dioMock);
+  final makeLogin = MakeLoginImpl(_repository);
+  test("deve falhar ao fazer login", () async {
+    try {
+      await makeLogin(password: '123', username: '81998012983');
+    } on NotLoggedException catch (exeption) {
+      expect(exeption, isA<NotLoggedException>());
+    }
+  });
+}
