1d99caefb4adf5ee747b476b5b35aa1b57832a84
Harley Italo Soares Xavier <harleyitalo@hotmail.com>
2022-03-11 21:58:36 -0300

    adicionando a classe abstrata de repositorio

--- lib/modules/posts/domain/repositories/posts_repository.dart
+++ lib/modules/posts/domain/repositories/posts_repository.dart
@@ -2,5 +2,7 @@ import 'package:boticario_app/modules/posts/domain/models/post_model.dart';
 
 abstract class PostsRepository {
   Future<List<PostModel>> findAllPosts();
-  Future<PostModel> sendPost(PostModel postModel);
+  Future<PostModel> insertPost(PostModel postModel);
+  Future<PostModel> updatePost(String id, PostModel postModel);
+  Future<bool> deletePost(String id);
 }
